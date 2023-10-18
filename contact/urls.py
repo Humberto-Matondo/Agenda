2f57dac0d86fc5b9f357dad2fda222d64c3d238e
Humberto-Matondo <humbertomat99@gmail.com>
2023-10-18 22:33:29 +0100

    created the contact profile

--- contact/urls.py
+++ contact/urls.py
@@ -4,6 +4,7 @@ from contact import views
 app_name= 'contact'
 
 urlpatterns = [
+    path('<int:contact_id>/', views.contact, name='contact'), #<int:contact_id> - precisa receber um numero, e o numero sera o contact_id do CONTACT_VIEWS
     path('', views.index, name='index'),
 ]
-            
+
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from contact.models import Contact
+
 # Create your views here.
 def index(request):
 
@@ -11,5 +12,19 @@ def index(request):
     return render(
         request,
         'contact\index.html',
+        context,
+    )
+
+def contact(request, contact_id):
+
+    single_contact = get_object_or_404(Contact.objects.filter(id=contact_id,show=True)) # O get...404 pode-se manipular tmbm dessa forma: (Contact, pk=contact_id),  
+
+    context = {
+        'contact': single_contact, 
+    }
+
+    return render(
+        request,
+        'contact\contact.html',
         context,
     )
