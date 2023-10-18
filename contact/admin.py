bf35e67ad94c09dc198732ed9d2004b0adfc5653
Humberto-Matondo <humbertomat99@gmail.com>
2023-10-18 21:02:08 +0100

    filter contacts who have SHOW=TRUE

--- contact/admin.py
+++ contact/admin.py
@@ -6,12 +6,13 @@ from contact import models
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
     
-    list_display = 'id', 'first_name', 'last_name', 'email', 'phone',
-    ordering = 'id', 
+    list_display = 'id', 'first_name', 'last_name', 'email', 'phone', 'show', 
+    ordering = '-id', 
     search_fields = 'id', 'first_name', 'last_name',
     list_per_page = 10 
     list_max_show_all = 100 
     list_display_links = 'first_name', 'last_name', 'email', 'phone',
+    list_editable = 'show', #para poder editar os show de forma rapida 
 
     
 @admin.register(models.Category)
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -3,7 +3,7 @@ from contact.models import Contact
 # Create your views here.
 def index(request):
 
-    contacts = Contact.objects.all()
+    contacts = Contact.objects.filter(show=True).order_by('-id') 
     context = {
         'contacts': contacts, 
     }
