0ce3fc05369ccdfc6abad470fb25e465de4c3a88
Humberto-Matondo <humbertomat99@gmail.com>
2023-10-19 12:12:49 +0100

    Created partials file

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -7,6 +7,7 @@ def index(request):
     contacts = Contact.objects.filter(show=True).order_by('-id') 
     context = {
         'contacts': contacts, 
+        'site_title': 'Contactos - '
     }
 
     return render(
@@ -19,8 +20,11 @@ def contact(request, contact_id):
 
     single_contact = get_object_or_404(Contact.objects.filter(id=contact_id,show=True)) # O get...404 pode-se manipular tmbm dessa forma: (Contact, pk=contact_id),  
 
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
+
     context = {
         'contact': single_contact, 
+        'site_title': site_title,
     }
 
     return render(
