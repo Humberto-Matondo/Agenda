473cacefa8e2fdb3e1591b9d5108b0c01d520ae9
Humberto-Matondo <humbertomat99@gmail.com>
2023-10-21 23:53:16 +0100

    VALIDATING FORM FIELDS

--- contact/forms.py
+++ contact/forms.py
@@ -17,21 +17,32 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = models.Contact
         fields = ('first_name','last_name','phone',)
+ 
+    def clean(self):
 
-    def clean(self): 
-        
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro',
-               code='invalid'
-            )
-        )
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro 2',
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+
+        if first_name == last_name:
+            msg = ValidationError(
+                'segundo nome não pode ser igual ao primeiro',
                 code='invalid'
             )
-        )   
+            
+            self.add_error('last_name', msg)
+
         return super().clean()
+
+        def clean_first_name(self):
+            first_name = self.cleaned_data.get('first_name')
+
+            if first_name == 'ABC':
+                self.add_error(
+                    'first_name',
+                    ValidationError(
+                        'Veio do add_error', code='invalid'
+                )
+            )
+
+        return first_name
