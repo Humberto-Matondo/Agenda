f792e43753c7ccc3c11f951d46071101c06cdf26
Humberto-Matondo <humbertomat99@gmail.com>
2023-10-21 10:17:08 +0100

    created dinamic forms with forms.ModelForm

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -3,13 +3,54 @@ from django.db.models import Q
 from contact.models import Contact
 from django.core.paginator import Paginator
 
+from django import forms
+from django.core.exceptions import ValidationError
+class ContactForm(forms.ModelForm): #forms. - tem varios tipos de forms que posso utilizar
+    #ModelForm - e um form pageando-se nos models(que se encontram no arq models.py) que ja temos. 
+    ...
+    class Meta:
+        model = Contact
+        fields = ('first_name','last_name','phone',)
+
+    def clean(self): # class para mostrar os erros:
+        cleaned_data = self.cleaned_data
+
+        self.add_error(
+            'first_name',
+            ValidationError(
+                'Mensagem de erro',
+                code='invalid'
+            )
+        )
+        self.add_error(
+            'first_name',
+            ValidationError(
+                'Mensagem de erro 2',
+                code='invalid'
+            )
+        )
+        
+        return super().clean()
 
 def create(request):
 
+    #FORMULARIO COM POST: 
+    if request.method == 'POST':
+        context = {
+            'form':ContactForm(request.POST)
+        }
+
+        return render(
+            request,
+            'contact\create.html',
+            context,
+        )
+
+    #FORMULARIO LIMPO:
     context = {
-
+        'form':ContactForm()
     }
-
+    
     return render(
         request,
         'contact\create.html',
