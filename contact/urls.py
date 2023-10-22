d5c956305586616f1c8d25bd9148729af558eb92
Humberto-Matondo <humbertomat99@gmail.com>
2023-10-22 20:34:56 +0100

    CREATED VIEWS,URL and TEMPLATES TO DELETE CONTACTS

--- contact/urls.py
+++ contact/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('contact/create/', views.create, name='create'), 
 
     path('contact/<int:contact_id>/update/', views.update, name='update'), 
-    
+    path('contact/<int:contact_id>/delete/', views.delete, name='delete'), 
+
 ]
 
