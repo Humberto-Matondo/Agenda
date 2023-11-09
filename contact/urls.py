450f6e441c5883e8488c159573003004a70ee33b
Humberto-Matondo <humbertomat99@gmail.com>
2023-11-09 03:49:47 +0100

    change index url position

--- contact/urls.py
+++ contact/urls.py
@@ -5,8 +5,8 @@ from contact import views
 app_name= 'contact'
 
 urlpatterns = [
-    path('search/', views.search, name='search'),  
     path('', views.index, name='index'),
+    path('search/', views.search, name='search'),  
     
     #contact(CRUD)
     path('contact/<int:contact_id>/', views.contact, name='contact'), 
